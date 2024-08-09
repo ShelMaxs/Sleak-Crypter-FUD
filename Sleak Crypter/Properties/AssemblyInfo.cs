648f4d2eb75abff104fae965e1c06c83d37bd7de
Isaaac840 <hgdasyuhgd4@outlook.com>
2024-08-09 20:42:50 +0400

    Upload Files

---
+++ Sleak Crypter/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sleak Crypter Tool")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sleak Crypter Tool")]
+[assembly: AssemblyCopyright("© Sleak Corporation. All rights reserved")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("bed4fce1-8987-4c2e-9aaf-4dba493b393f")]
+[assembly: AssemblyVersion("2.7.0.0")]
+[assembly: AssemblyFileVersion("2.7.0.0")]
